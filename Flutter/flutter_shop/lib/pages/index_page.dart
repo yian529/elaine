4af3816704b875a3f9ede464d014931045478d3e
yeahmobi <yeahmobi@elaine.local>
2019-09-17 13:50:57 +0800

    增加底部导航栏

--- Flutter/flutter_shop/lib/pages/index_page.dart
+++ Flutter/flutter_shop/lib/pages/index_page.dart
@@ -1,17 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './card_page.dart';
+import './category_page.dart';
+import './home_page.dart';
+import './member_page.dart';
 
-class IndexPage extends StatelessWidget {
-  const IndexPage({Key key}) : super(key: key);
+class IndexPage extends StatefulWidget {
+  IndexPage({Key key}) : super(key: key);
+
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车')
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('会员中心')
+    )
+  ];
+
+  final List TabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CatrdPage(),
+    MemberPage()
+  ];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    currentPage = TabBodies[0];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("百姓生活+"),
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            currentPage = TabBodies[currentIndex];
+          });
+        } ,
       ),
-      body: Center(
-        child: Text("百姓生活+"),
-      ),
+      body: currentPage,
     );
   }
 }
